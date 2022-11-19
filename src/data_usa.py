1bc977ea9601efe2d913d8135ecae6f05342dec5
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:42:14 +0800

    Update data_usa.py

--- src/data_usa.py
+++ src/data_usa.py
@@ -1,8 +1,7 @@
 from requests import get
 
-
 class DataUSA:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://datausa.io/api"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
@@ -12,7 +11,7 @@ class DataUSA:
 			self,
 			drill_downs: str,
 			measures: str,
-			year: str = "latest"):
+			year: str = "latest") -> dict:
 		return get(
 			f"{self.api}/data?drilldowns={drill_downs}&measures={measures}&year={year}",
 			headers=self.headers).json()
